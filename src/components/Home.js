b61647a81a0331f610cb583a7a79027603dd46b9
theakhiljha <Akhilkumarjha9@gmail.com>
2021-04-21 12:42:46 +0530

    update in code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'Sample Post TItle 1', body: 'blah bah blah....', author: 'Akhil', id: 1},
+        { title: 'Sample Post Title 2', body: 'Hey there....', author: 'Akhil', id: 2},
+        { title: 'Sample Post Title 3', body: 'la la la la la laa la ....', author: 'Akhil', id: 3}
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            <BlogList blogs={blogs} title={'All Blogs !'}/>
         </div>
      );
 }
